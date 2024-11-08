b14f3b7ed5cbec20786bae3fe85008b9c3bf29a2
Subhash <sprajapat160599@gmail.com>
2024-11-08 13:49:13 +0530

    range() function practice

---
+++ Loop/usage_of_range.py
@@ -0,0 +1,39 @@
+# First Use
+for a in range(9):
+  print(a)
+  
+print()
+
+# Second  Use
+for a in range(2,11):
+  print(a)
+
+print()
+
+# third use
+for b in range(3,25,5):
+  print(b)
+print()
+
+# Fourth Use
+
+for n in range(8):
+  print("Hello Universe!")
+print()
+
+# Fifth use
+# create multiplication table with the help of range() in for loop
+for a in range(1,11):
+  print(2*a)
+print()
+
+# Sixth Use
+for a in range(1,11):
+  print("2*",a ,"=",2*a)
+print()
+
+# Seventh Use
+#Reverse case of range() function
+
+for w in range(11, 1,-2):
+  print(w)
